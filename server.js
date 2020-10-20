61d2af1d3adaf4d4a90fb7e79e01861aff879878
natepines <natelpines@verizon.net>
2020-10-20 18:20:16 -0400

    lab 6

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.route('/api')
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
 
-    res.send("Hello World");
+    res.join(countries);
   });
 
 app.listen(port, () => {
